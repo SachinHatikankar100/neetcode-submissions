981b806452b7ee8cd77d06df5d8d1762450272f6
Sachin Hatikankan <shatikankan@gmail.com>
2026-09-01 00:53:33 +0530

    Add: same-binary-tree - submission-4

---
+++ Data Structures & Algorithms/same-binary-tree/submission-4.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        #Depth First Search Approach
+        # if not p and not q:
+        #     return True
+        
+        # if p and q and p.val == q.val:
+        #     return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # else:
+        #     return False
+
+        #Iterative DFS
+        # stack = [(p,q)]
+        # while stack:
+        #     node1, node2 = stack.pop()
+        #     if not node1 and not node2:
+        #         continue
+        #     if not node1 or not node2 or node1.val != node2.val:
+        #         return False
+        #     stack.append((node1.right, node2.right))
+        #     stack.append((node1.left,node2.left))
+        # return True
+
+        #Bread First Search Approach
+        q1 = deque([p])
+        q2 = deque([q])
+        while q1 and q2:
+            for _ in range(len(q1)):
+                nodeP = q1.popleft()
+                nodeQ = q2.popleft()
+                if nodeP is None and nodeQ is None:
+                    continue
+                if nodeP is None or nodeQ is None or nodeP.val != nodeQ.val:
+                    return False
+                q1.append(nodeP.left)
+                q1.append(nodeP.right)
+                q2.append(nodeQ.left)
+                q2.append(nodeQ.right)
+        return True

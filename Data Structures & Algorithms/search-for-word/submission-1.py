0b2ce047a2d5c948b557dd7aa0d6d2dff30744c1
Sachin Hatikankan <shatikankan@gmail.com>
2026-09-07 18:18:38 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,48 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        #Backtracking Hashset approach
+        # ROWS , COLS = len(board), len(board[0])
+        # path = set()
+
+        # def dfs(r,c,i):
+        #     if i == len(word):
+        #         return True
+        #     if (min(r,c)<0 or r>= ROWS or c >= COLS or word[i] != board[r][c] or(r,c) in path):
+        #         return False
+
+        #     path.add((r,c))
+        #     res = (dfs(r+1,c,i+1)or
+        #             dfs(r-1,c,i+1)or
+        #             dfs(r,c+1,i+1)or
+        #             dfs(r,c-1,i+1))
+        #     path.remove((r,c))
+        #     return res
+
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if dfs(r,c,0):
+        #             return True
+        # return False
+
+        # Backtracking Visited Array Approach
+        ROWS, COLS = len(board), len(board[0])
+        visited = [[False for _ in range(COLS)] for _ in range(ROWS)]
+        def dfs(r,c,i):
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or r>= ROWS or c>= COLS or word[i]!=board[r][c] or visited[r][c]):
+                return False
+
+            visited[r][c] = True
+            res = (dfs(r+1, c, i+1) or
+                    dfs(r-1, c, i+1) or
+                    dfs(r, c+1, i+1) or
+                    dfs(r, c-1, i+1))
+            visited[r][c] = False
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r,c,0):
+                    return True
+        return False

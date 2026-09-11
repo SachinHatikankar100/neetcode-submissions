7731f17f80b77fc90a6d89d3de8bef7dbe0472ee
Sachin Hatikankan <shatikankan@gmail.com>
2026-09-11 16:42:33 +0530

    Add: insert-new-interval - submission-4

---
+++ Data Structures & Algorithms/insert-new-interval/submission-4.py
@@ -0,0 +1,60 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        #Linear Search Approach
+        # n = len(intervals)
+        # i = 0
+        # res = []
+        # while i < n and intervals[i][1] < newInterval[0]:
+        #     res.append(intervals[i])
+        #     i += 1
+        # while i < n and newInterval[1] >= intervals[i][0]:
+        #     newInterval[0] = min(newInterval[0], intervals[i][0])
+        #     newInterval[1] = max(newInterval[1], intervals[i][1])
+        #     i += 1
+        # res.append(newInterval)
+
+        # while i < n:
+        #     res.append(intervals[i])
+        #     i += 1
+
+        # return res
+
+        #Binary Search Approach
+        # if not intervals:
+        #     return [newInterval]
+        # n = len(intervals)
+        # target = newInterval[0]
+        # left, right = 0, n-1
+
+        # while left<= right:
+        #     mid = (left+right)//2
+        #     if intervals[mid][0]< target:
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
+        # intervals.insert(left, newInterval)
+        
+        # res = []
+        # for interval in intervals:
+        #     if not res or res[-1][1] < interval[0]:
+        #         res.append(interval)
+        #     else:
+        #         res[-1][1] = max(res[-1][1], interval[1])
+        # return res
+
+
+        #Greedy Approach
+        res = []
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1])
+                ]
+        res.append(newInterval)
+        return res

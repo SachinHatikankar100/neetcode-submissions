830065de57f5cbd9824592e48436a7ddd37517c5
Sachin Hatikankan <shatikankan@gmail.com>
2026-09-14 00:07:34 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.py
@@ -0,0 +1,33 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        #Brute Force Approach
+        n = len(intervals)
+        for i in range(n):
+            A = intervals[i]
+            for j in range(i+1, n):
+                B = intervals[j]
+
+                if min(A.end, B.end) > max(A.start, B.start):
+                    return False
+        return True
+
+
+        #Sorting Approach
+
+        # intervals.sort(key=lambda i:i.start)
+        # for i in range(1, len(intervals)):
+        #     i1 = intervals[i-1]
+        #     i2 = intervals[i]
+
+        #     if i1.end > i2.start:
+        #         return False
+        # return True
+

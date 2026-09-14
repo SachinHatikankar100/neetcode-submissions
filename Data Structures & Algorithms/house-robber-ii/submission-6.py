f49c7f57d5f3886986ea086f52a0962127d84a4f
Sachin Hatikankan <shatikankan@gmail.com>
2026-09-14 18:59:58 +0530

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.py
@@ -0,0 +1,36 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        #Recursion Approach
+        # if len(nums)==1:
+        #     return 0
+        # def dfs(i, flag):
+        #     if i >= len(nums) or (flag and len(nums)-1):
+        #         return 0
+        #     return max(dfs(i+1, flag), nums[i]+dfs(i+2, flag or i==0))
+        
+        # return max(dfs(0,True), dfs(1, False))
+        #Dynamic Top down approach
+        if len(nums)==1:
+            return nums[0]
+        memo = [[-1]*2 for _ in range(len(nums))]
+        def dfs(i, flag):
+            if i >= len(nums) or (flag and i == len(nums)-1):
+                return 0
+            if memo[i][flag] != -1:
+                return memo[i][flag]
+            memo[i][flag] = max(dfs(i+1, flag), nums[i]+ dfs(i+2, flag or (i==0)))
+            return memo[i][flag]
+        return max(dfs(0, True), dfs(1, False))
+        #Dynamic Bottom up approach
+
+
+
+        #Dynamic Space optimized approach
+    #     return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+    # def helper(self, nums):
+    #     rob1, rob2 = 0, 0
+    #     for num in nums:
+    #         newRob = max(rob1+num, rob2)
+    #         rob1 = rob2
+    #         rob2 = newRob
+    #     return rob2

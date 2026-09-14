ac3d2593393e2667a1b53ca800c0e9af14b50321
Sachin Hatikankan <shatikankan@gmail.com>
2026-09-14 14:59:35 +0530

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,46 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #Recursion Approach
+        # def dfs(i):
+        #     if i >= n:
+        #         return i==n
+        #     return dfs(i+1) + dfs(i+2)
+        # return dfs(0)
+
+        #Dynamic Programming Top down approach
+        # cache = [-1] * n
+        # def dfs(i):
+        #     if i >= n:
+        #         return i ==n
+        #     if cache[i] != -1:
+        #         return cache[i]
+        #     cache[i] = dfs(i+1) + dfs(i+2)
+        #     return cache[i]
+        # return dfs(0)
+
+        #Dynamic Programming Bottom up approach
+        if n<= 2:
+            return n
+        dp = [0] * (n+1)
+        dp[1], dp[2]= 1, 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+        #Dynamic Programming Space optimized approach
+        # one, two = 1, 1
+        # for i in range(n-1):
+        #     temp = one
+        #     one = one + two
+        #     two = temp
+
+        # return one
+
+
+        #Maths Approach
+
+        # sqrt5 = math.sqrt(5)
+        # phi = (1 + sqrt5)/2
+        # psi = (1 - sqrt5)/2
+        # n += 1
+        # return round((phi**n - psi**n)/sqrt5)
